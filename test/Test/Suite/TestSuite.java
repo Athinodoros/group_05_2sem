0396fd6e9e1d734ba55f623c58e7631ee72f6159
Bo-Vilstrup <bo.vilstrup@gmail.com>
2015-04-09 20:54:32 +0200

    adding a test suite to junit test

---
+++ test/Test/Suite/TestSuite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test.Suite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author bo
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    
+    layer3.dataSource.DBConnectorTest.class
+})
+public class TestSuite {
+    
+}
