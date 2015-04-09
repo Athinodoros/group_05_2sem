c678e021abc129b9276677a9de4e24ef148cdbf1
Bo-Vilstrup <bo.vilstrup@gmail.com>
2015-04-10 01:25:27 +0200

    adding more mappers to layer3

--- src/java/layer3/dataSource/mapper/CompanyManager.java
+++ src/java/layer3/dataSource/mapper/CompanyManager.java
@@ -5,10 +5,71 @@
  */
 package layer3.dataSource.mapper;
 
+import java.sql.*;
+import layer2.domain.bean.Company;
+import layer3.dataSource.DBConnector;
+
 /**
  *
  * @author bo
  */
 public class CompanyManager {
+    
+     public boolean insert(Connection conn, Company bean) { 
+
+        int rowsInserted = 0;
+        
+         String sql1 = "INSERT into companies (companyname, budget) "
+                + "VALUES (?, ?)";
+        
+        // This code is taken from Henrik's DataSourceLayerDemo :: Class OrderMapper.jave
+        //-------------------------------------------------------------------------------
+        String sql2
+                = "select projectSequence.nextval " //= "select orderseq.nextval  "
+                + "from dual";                      // <- this (dual) is a dummy table, and it is needed
+                                                    // because of the select statemant (oracle data-base only)
+        //--------------------------------------------------------------------------------
+        
+        ResultSet keys = null;
+        try (
+                PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
+                
+                // Henrik's code (needed to update primary key (oracle data-base only)->
+                PreparedStatement stmt1 = conn.prepareStatement( sql2 );
+                ) {
+
+            
+//            //== (Henrik's code) get unique key
+//            //--------------------------------
+//            keys = stmt1.executeQuery();
+//            if (keys.next()) {
+//                bean.setUserID(keys.getInt(1)); // <- get the key from dummy table (dual)
+//            }
+//            else {
+//                // ToDo - 
+//            }
+            //--------------------------------
+     
+            //== insert tuple
+            stmt.setString  (1, bean.getCompanyName());
+            stmt.setInt     (2, bean.getBudget());       
+            rowsInserted  = stmt.executeUpdate();
+            
+
+        } catch (SQLException e) {
+            DBConnector.processException(e);
+            return false;
+        } finally {
+            if (keys != null) {
+                try {
+                    keys.close();
+                } catch (SQLException e) {
+                    DBConnector.processException(e);
+                }
+            }
+        }
+        return rowsInserted == 1;
+    } // End of method :: insert()
+    
     
 }
--- src/java/layer3/dataSource/mapper/UserManager.java
+++ src/java/layer3/dataSource/mapper/UserManager.java
@@ -5,10 +5,77 @@
  */
 package layer3.dataSource.mapper;
 
+import java.sql.*;
+import layer2.domain.bean.User;
+import layer3.dataSource.DBConnector;
+import layer3.dataSource.utility.Convert;
+
 /**
  *
  * @author bo
  */
 public class UserManager {
+    
+     public boolean insert(Connection conn, User bean) { 
+
+        int rowsInserted = 0;
+        
+         String sql1 = "INSERT into projects (userid, uname, password, email, country, urole, company) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        
+        // This code is taken from Henrik's DataSourceLayerDemo :: Class OrderMapper.jave
+        //-------------------------------------------------------------------------------
+        String sql2
+                = "select projectSequence.nextval " //= "select orderseq.nextval  "
+                + "from dual";                      // <- this (dual) is a dummy table, and it is needed
+                                                    // because of the select statemant (oracle data-base only)
+        //--------------------------------------------------------------------------------
+        
+        ResultSet keys = null;
+        try (
+                PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
+                
+                // Henrik's code (needed to update primary key (oracle data-base only)->
+                PreparedStatement stmt1 = conn.prepareStatement( sql2 );
+                ) {
+
+            
+            //== (Henrik's code) get unique key
+            //--------------------------------
+            keys = stmt1.executeQuery();
+            if (keys.next()) {
+                bean.setUserID(keys.getInt(1)); // <- get the key from dummy table (dual)
+            }
+            else {
+                // ToDo - 
+            }
+            //--------------------------------
+     
+            //== insert tuple
+            stmt.setInt     (1, bean.getUserID());
+            stmt.setString  (2, bean.getName());
+            stmt.setString  (3, bean.getPassword());
+            stmt.setString  (4, bean.getEmail());
+            stmt.setString  (5, bean.getCountry());
+            stmt.setString  (6, bean.getRole());
+            stmt.setString  (7, bean.getCompany().getCompanyName());        
+            rowsInserted  = stmt.executeUpdate();
+            
+
+        } catch (SQLException e) {
+            DBConnector.processException(e);
+            return false;
+        } finally {
+            if (keys != null) {
+                try {
+                    keys.close();
+                } catch (SQLException e) {
+                    DBConnector.processException(e);
+                }
+            }
+        }
+        return rowsInserted == 1;
+    } // End of method :: insert()
+    
     
 }
