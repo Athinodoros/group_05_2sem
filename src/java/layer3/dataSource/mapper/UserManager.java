884ff12fcfcaafebe9381f16e5a8676abd701a7b
Bo-Vilstrup <bo.vilstrup@gmail.com>
2015-04-13 12:22:58 +0200

    adding some getRow method to UserManager

--- src/java/layer3/dataSource/mapper/UserManager.java
+++ src/java/layer3/dataSource/mapper/UserManager.java
@@ -20,7 +20,7 @@ public class UserManager {
 
         int rowsInserted = 0;
         
-         String sql1 = "INSERT into projects (userid, uname, password, email, country, urole, company) "
+         String sql1 = "INSERT into users (userid, uname, password, email, country, urole, company) "
                 + "VALUES (?, ?, ?, ?, ?, ?, ?)";
         
         // This code is taken from Henrik's DataSourceLayerDemo :: Class OrderMapper.jave
@@ -30,16 +30,14 @@ public class UserManager {
                 + "from dual";                      // <- this (dual) is a dummy table, and it is needed
                                                     // because of the select statemant (oracle data-base only)
         //--------------------------------------------------------------------------------
-        
         ResultSet keys = null;
         try (
-                PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
-                
+                //PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
+                PreparedStatement stmt = conn.prepareStatement(sql1 );
                 // Henrik's code (needed to update primary key (oracle data-base only)->
                 PreparedStatement stmt1 = conn.prepareStatement( sql2 );
                 ) {
 
-            
             //== (Henrik's code) get unique key
             //--------------------------------
             keys = stmt1.executeQuery();
@@ -78,4 +76,45 @@ public class UserManager {
     } // End of method :: insert()
     
     
-}
+    public User getRow(Connection conn, int userid) { 
+
+        String sql = "SELECT * FROM users WHERE userid = ?";
+        ResultSet rs = null;
+
+        try ( PreparedStatement stmt = conn.prepareStatement(sql); ) {
+            
+            stmt.setInt(1, userid);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                User bean = new User();
+                System.out.println(bean.toString());
+                bean.setUserID(userid);
+                bean.setName(rs.getNString("uname"));
+                bean.setPassword(rs.getNString("password"));
+                bean.setEmail(rs.getNString("email"));
+                bean.setCountry(rs.getNString("country"));
+                bean.setRole(rs.getNString("urole"));
+                bean.getCompany().setCompanyName(rs.getNString("company"));
+               
+                
+                return bean;
+            } else {
+                return null;
+            }
+
+        } catch ( SQLException e) {
+            DBConnector.processException(e);
+            return null;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    DBConnector.processException(e);
+                }
+            }
+        }
+    } // End of method :: getRow() 
+    
+} // End of Class :: UserManager
