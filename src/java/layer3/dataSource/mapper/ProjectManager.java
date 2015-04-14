6af7db69857523ac6dbecd11c17f0514826d1b19
Bancho Petrov <bancho_p@abv.bg>
2015-04-14 15:02:22 +0200

    made progress in ProjectManager
    getting a weird AssertionFailed error in ProjectManagerTest, when I can
    clearly see that the two strings I am comparing are identical

--- src/java/layer3/dataSource/mapper/ProjectManager.java
+++ src/java/layer3/dataSource/mapper/ProjectManager.java
@@ -6,7 +6,10 @@
 package layer3.dataSource.mapper;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import layer2.domain.bean.Project;
+import layer2.domain.bean.User;
 import layer3.dataSource.DBConnector;
 import layer3.dataSource.utility.Convert;
 
@@ -15,54 +18,49 @@ import layer3.dataSource.utility.Convert;
  * @author bo
  */
 public class ProjectManager {
-    
-    public boolean insert(Connection conn, Project bean) { 
+
+    public boolean insert(Connection conn, Project bean) {
 
         int rowsInserted = 0;
-        
-         String sql1 = "INSERT into projects (projectid, authorid, title, startdate, enddate, stage, budget, poe, comments) "
+
+        String sql1 = "INSERT into projects (projectid, authorid, title, startdate, enddate, stage, budget, poe, comments) "
                 + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        
+
         // This code is taken from Henrik's DataSourceLayerDemo :: Class OrderMapper.jave
         //-------------------------------------------------------------------------------
         String sql2
                 = "select projectSequence.nextval " //= "select orderseq.nextval  "
                 + "from dual";                      // <- this (dual) is a dummy table, and it is needed
-                                                    // because of the select statemant (oracle data-base only)
+        // because of the select statemant (oracle data-base only)
         //--------------------------------------------------------------------------------
-        
+
         ResultSet keys = null;
         try (
                 PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
-                
-                // Henrik's code (needed to update primary key (oracle data-base only)->
-                PreparedStatement stmt1 = conn.prepareStatement( sql2 );
-                ) {
+                PreparedStatement stmt1 = conn.prepareStatement(sql2);) {
 
-            
             //== (Henrik's code) get unique key
             //--------------------------------
             keys = stmt1.executeQuery();
             if (keys.next()) {
                 bean.setProjectID(keys.getInt(1)); // <- get the key from dummy table (dual)
-            }
-            else {
+            } else {
                 // ToDo - 
             }
             //--------------------------------
-     
-            //== insert tuple
-            stmt.setInt     (1, bean.getProjectID());
-            stmt.setNull    (2, bean.getAuthor());
-            stmt.setString  (3, bean.getTitle());
-            stmt.setDate    (4, Convert.date2SqlDate(bean.getStartDate()));
-            stmt.setDate    (5, Convert.date2SqlDate(bean.getEndDate()));
-            stmt.setString  (6, bean.getStage());
-            stmt.setInt     (7, bean.getBudget());
-            stmt.setString  (8, Convert.Boolean2String(bean.hasPOE()));
-            stmt.setString  (9, bean.getComments());
-            rowsInserted  = stmt.executeUpdate();
+
             
+            //== insert tuple
+            stmt.setInt(1, bean.getProjectID());
+            stmt.setInt(2, bean.getAuthor().getUserID());
+            stmt.setString(3, bean.getTitle());
+            stmt.setDate(4, Convert.date2SqlDate(bean.getStartDate()));
+            stmt.setDate(5, Convert.date2SqlDate(bean.getEndDate()));
+            stmt.setString(6, bean.getStage());
+            stmt.setInt(7, bean.getBudget());
+            stmt.setString(8, Convert.Boolean2String(bean.hasPOE()));
+            stmt.setString(9, bean.getComments());
+            rowsInserted = stmt.executeUpdate();
 
         } catch (SQLException e) {
             DBConnector.processException(e);
@@ -78,4 +76,47 @@ public class ProjectManager {
         }
         return rowsInserted == 1;
     } // End of method :: insert()
+    
+    
+    public Project getRow(Connection conn, int projectid){
+        
+        String sql = "SELECT * FROM projects WHERE projectid = ?";
+        ResultSet rs = null;
+        Project bean = new Project();
+        
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, projectid);
+            rs = stmt.executeQuery();
+            
+            if (rs.next()) {
+                bean.setProjectID(rs.getInt("projectid"));
+                bean.setTitle(rs.getNString("title"));
+                bean.setStartDate(Convert.sqlDate2Date(rs.getDate("startdate")));
+                bean.setEndDate(Convert.sqlDate2Date(rs.getDate("enddate")));
+                bean.setStage(rs.getNString("stage"));
+                bean.setBudget(rs.getInt("budget"));
+                bean.setPOE(Convert.String2Boolean(rs.getNString("poe")));
+                bean.setComments(rs.getNString("comments"));
+                
+                int authorID = rs.getInt("authorid");
+                UserManager userManager = new UserManager();
+                User author = userManager.getRow(conn, authorID);
+                bean.setAuthor(author);
+            }
+            
+            return bean;
+            
+        } catch (SQLException e) {
+            DBConnector.processException(e);
+            return null;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    DBConnector.processException(ex);
+                }
+            }
+        }
+    } // End of method :: getRow()
 } // End of Class :: ProjectManager
--- test/layer3/dataSource/mapper/ProjectManagerTest.java
+++ test/layer3/dataSource/mapper/ProjectManagerTest.java
@@ -6,8 +6,14 @@
 package layer3.dataSource.mapper;
 
 import java.sql.Connection;
+import java.util.Calendar;
+import static java.util.Calendar.MONTH;
 import java.util.Date;
+import java.util.Random;
+import layer2.domain.bean.Company;
 import layer2.domain.bean.Project;
+import layer2.domain.bean.User;
+import layer2.domain.interfaces.NamingConv;
 import layer3.dataSource.DBConnector;
 import layer3.dataSource.DBType;
 import org.junit.After;
@@ -22,29 +28,56 @@ import static org.junit.Assert.*;
  * @author bo
  */
 public class ProjectManagerTest {
-    
+
     private static Connection conn;
+
+    private ProjectManager projectManager;
+    private UserManager userManager;
+    private CompanyManager companyManager;
     
+    private Project project;
+    private User author;
+    private Company dummyCompany;
+    private final Date startDate;
+    private final Date endDate;
+    
+
     public ProjectManagerTest() {
+        Calendar cal = Calendar.getInstance();
+        Date today = cal.getTime();
+        startDate = today;
+        
+        cal.add(MONTH, 1);
+        Date oneMonthLater = cal.getTime();
+        endDate = oneMonthLater;
+        
+        dummyCompany = new Company("dummy_" + new Random().nextInt(), 42);
+        author = new User(0, "Bob", "PassWordTest", "test@sth.whatever", "Testlandia", NamingConv.RESELLER, dummyCompany);
+
+        project = new Project(0, author, ">>> Title test <<<", startDate, endDate, ">>> Stage test <<<", 42, false, ">>> This is a test <<<");
+        
+        projectManager = new ProjectManager();
+        userManager = new UserManager();
+        companyManager = new CompanyManager();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
-        
+
         DBConnector.getInstance().setDBType(DBType.ORACLE_THIN);
         conn = DBConnector.getInstance().getConnection();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-        
+
         DBConnector.getInstance().close();
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -54,39 +87,43 @@ public class ProjectManagerTest {
      */
     @Test
     public void testInsert() {
-        System.out.println("insert");
-    
+        System.out.println("Testing :: ProjectManager.insert()");
+
+        //making sure I insert all the necessary rows in the other tables first
+        boolean status1 = companyManager.insert(conn, dummyCompany);
+        boolean status2 = userManager.insert(conn, author);
         
+        boolean result = false;
         
-        final int       projectID   = 0;
-        final int       author      = 0;
-        final String    title       = ">>> Title test <<<";
-        final Date      startDate   = new Date();
-        final Date      endDate     = new Date();
-        final String    stage       = ">>> Stage test <<<";
-        final int       budget      = 0;
-        final boolean   hasPOE      = false;
-        final String    comments    = ">>> This is a test <<<";
-    
+        if (status1 && status2) {
+            result = projectManager.insert(conn, project);
+        }
         
-        Project bean = new Project(
-            projectID,
-            author,
-            title,
-            startDate,
-            endDate,
-            stage,
-            budget,
-            hasPOE,
-            comments
-        );
-        
-        ProjectManager instance = new ProjectManager();
-        
-        boolean expResult = true;
-        boolean result = instance.insert(conn, bean);
-        assertEquals(expResult, result);
-       
+        assertTrue(result);
+
     } // End of method :: testInsert()
+
     
+    @Test
+    public void testGetRow() {
+        System.out.println("Testing :: ProjectManager.getRow()");
+        
+        //making sure I first insert all the necessary rows, before I try to get something out
+        boolean status1 = companyManager.insert(conn, dummyCompany);
+        boolean status2 = userManager.insert(conn, author);
+        boolean status3 = projectManager.insert(conn, project);
+        
+        Project result = null;
+        if (status1 && status2 && status3) {
+            result = projectManager.getRow(conn, project.getProjectID());
+        }
+        if (result != null) {
+            System.out.println("Expected");
+            System.out.println(project.toString());
+            System.out.println("Result");
+            System.out.println(result.toString());
+            assertTrue(result.toString().equals(project.toString()));
+        }
+        
+    } // End of method :: testGetRow()
 } // End of class :: 
