622a5b6be101d337b273dafab130af66bdb9b9db
Alexander <alexandar.g@abv.bg>
2015-04-23 20:55:28 +0300

    95% ready Project Manager
    hello

--- src/java/layer3/dataSource/mapper/ProjectManager.java
+++ src/java/layer3/dataSource/mapper/ProjectManager.java
@@ -78,7 +78,7 @@ public class ProjectManager {
     
 //    public Project getRow(Connection conn, int projectid){
 //        
-//        String sql = "SELECT * FROM projects WHERE projectid = ?";
+//        String sql = "SELECT * FROM project WHERE projectid = ?";
 //        ResultSet rs = null;
 //        Project bean = new Project();
 //        
@@ -89,12 +89,15 @@ public class ProjectManager {
 //            if (rs.next()) {
 //                bean.setProjectID(rs.getInt("projectid"));
 //                bean.setTitle(rs.getNString("title"));
-//                bean.setStartDate(Convert.sqlDate2Date(rs.getDate("startdate")));
-//                bean.setEndDate(Convert.sqlDate2Date(rs.getDate("enddate")));
+//                //bean.setFdate();
+//                //bean.setEndDate(Convert.sqlDate2Date(rs.getDate("enddate")));
 //                bean.setStage(rs.getNString("stage"));
-//                bean.setBudget(rs.getInt("budget"));
-//                bean.setPOE(Convert.string2Boolean(rs.getNString("poe")));
-//                bean.setComments(rs.getNString("comments"));
+//                bean.setProjectBudget(rs.getInt("budget"));
+//                bean.setCompanyName(rs.getNString("companyName"));
+//                bean.setDescription(rs.getNString("description"));
+    
+    
+//    
 //                
 //                int authorID = rs.getInt("authorid");
 //                UserManager userManager = new UserManager();
@@ -120,7 +123,7 @@ public class ProjectManager {
 //    
 //    
 //    public boolean delete(Connection conn, int projectid){
-//        String sql = "DELETE FROM projects WHERE projectid = ?";
+//        String sql = "DELETE FROM project WHERE projectid = ?";
 //        
 //        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 //            stmt.setInt(1, projectid);
@@ -143,12 +146,14 @@ public class ProjectManager {
 //        String sql = "UPDATE projects SET enddate = ? , stage = ? , budget = ? , poe = ? , comments = ? WHERE projectid = ?";
 //        
 //        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-//            stmt.setDate(1, Convert.date2SqlDate(bean.getEndDate()));
-//            stmt.setNString(2, bean.getStage());
-//            stmt.setInt(3, bean.getBudget());
-//            stmt.setNString(4, Convert.boolean2String(bean.hasPOE()));
-//            stmt.setNString(5, bean.getComments());
-//            stmt.setInt(6, bean.getProjectID());
+//            stmt.setInt(1, bean.getProjectID());
+//            stmt.setString(2, bean.getCompanyName());
+//            stmt.setString(3, bean.getTitle());
+//            stmt.setString(4, bean.getDescription());
+//            stmt.setString(5, bean.getStage());
+//            stmt.setDate(6, Convert.date2SqlDate(bean.getSdate()));
+//            stmt.setDate(7, Convert.date2SqlDate(bean.getFdate()));
+//            stmt.setInt(8, bean.getProjectBudget()); 
 //            
 //            int affected = stmt.executeUpdate();
 //            if (affected == 1) {
@@ -162,49 +167,49 @@ public class ProjectManager {
 //            return false;
 //        }
 //    }
-//    public ArrayList<Project> getAllRows(Connection conn){
-//        
-//        String sql = "SELECT * FROM projects";
-//        ResultSet rs = null;
-//        
-//        ArrayList<Project> allproj = new ArrayList<Project>();
-//        
-//        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-//            //stmt.setInt(1, projectid);
-//            rs = stmt.executeQuery();
-//            
-//            while (rs.next()) {
-//                Project bean = new Project();
-//                bean.setProjectID(rs.getInt("projectid"));
-//                bean.setTitle(rs.getNString("title"));
-//                bean.setStartDate(Convert.sqlDate2Date(rs.getDate("startdate")));
-//                bean.setEndDate(Convert.sqlDate2Date(rs.getDate("enddate")));
-//                bean.setStage(rs.getNString("stage"));
-//                bean.setBudget(rs.getInt("budget"));
-//                bean.setPOE(Convert.string2Boolean(rs.getNString("poe")));
-//                bean.setComments(rs.getNString("comments"));
-//                
-//                int authorID = rs.getInt("authorid");
-//                UserManager userManager = new UserManager();
-//                User author = userManager.getRow(conn, authorID);
-//                bean.setAuthor(author);
-//                allproj.add(bean);
-//            }
-//            return allproj;
-//            
-//            
-//        } catch (SQLException e) {
-//            DBConnector.processException(e);
-//            return null;
-//        } finally {
-//            if (rs != null) {
-//                try {
-//                    rs.close();
-//                } catch (SQLException ex) {
-//                    DBConnector.processException(ex);
-//                }
-//            }
-//        }
-    //}// End of method :: update()
+    public ArrayList<Project> getAllRows(Connection conn){
+        
+        String sql = "SELECT * FROM project";
+        ResultSet rs = null;
+        
+        ArrayList<Project> allproj = new ArrayList<Project>();
+        
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            //stmt.setInt(1, projectid);
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Project bean = new Project();
+                bean.setProjectID(rs.getInt("projectid"));
+                bean.setTitle(rs.getNString("title"));
+                //bean.setFdate();
+                //bean.setEndDate(Convert.sqlDate2Date(rs.getDate("enddate")));
+                bean.setStage(rs.getNString("stage"));
+                bean.setProjectBudget(rs.getInt("budget"));
+                bean.setCompanyName(rs.getNString("companyName"));
+                bean.setDescription(rs.getNString("description"));
+                
+                int authorID = rs.getInt("authorid");
+                UserManager userManager = new UserManager();
+                User author = userManager.getRow(conn, authorID);
+                //bean.setAuthor(author);
+                allproj.add(bean);
+            }
+            return allproj;
+            
+            
+        } catch (SQLException e) {
+            DBConnector.processException(e);
+            return null;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    DBConnector.processException(ex);
+                }
+            }
+        }
+    }// End of method :: update()
 } // End of Class :: ProjectManager+
 
