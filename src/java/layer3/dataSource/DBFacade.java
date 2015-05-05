b61a52dfc5de8659c43960124037fb934ea9b85f
Bo-Vilstrup <bo.vilstrup@gmail.com>
2015-05-05 11:05:39 +0200

    changing how to close and open a new connection in DBFacade

--- src/java/layer3/dataSource/DBFacade.java
+++ src/java/layer3/dataSource/DBFacade.java
@@ -27,7 +27,7 @@ import layer3.dataSource.mapper.UserInfoManager;
  */
 public class DBFacade {
 
-    private Connection conn;
+    private static Connection conn;
     
     private ProjectManager projectManager;
     private UserInfoManager userInfoManager;
@@ -50,6 +50,7 @@ public class DBFacade {
     }
 
     public static DBFacade getInstance() {
+        conn = DBConnector.getInstance().getConnection();
         return DBFacadeHolder.INSTANCE;
     }
 
@@ -58,63 +59,74 @@ public class DBFacade {
     }
 // <-- End of Singleton
     
-    private Connection getConnection(){
-        try {
-            if (conn.isClosed()) {
-                conn = DBConnector.getInstance().getConnection();
-            }
-        } catch (SQLException ex) {
-            DBConnector.processException(ex);
-        }
-        return conn;
-    }
+//    private Connection getConnection(){
+//        try {
+//            if (conn.isClosed()) {
+//                conn = DBConnector.getInstance().getConnection();
+//            }
+//        } catch (SQLException ex) {
+//            DBConnector.processException(ex);
+//        }
+//        return conn;
+//    }
     
     public void closeConnection() {
         DBConnector.getInstance().close();
     }
     
     public boolean createProject(Project newProject) {
-        return projectManager.insert(getConnection(), newProject);
+//        return projectManager.insert(getConnection(), newProject);
+        return projectManager.insert(conn, newProject);
     }
 
     public Project getProject(int projectid) {
-        return projectManager.getRow(getConnection(), projectid);
+//        return projectManager.getRow(getConnection(), projectid);
+        return projectManager.getRow(conn, projectid);
     }
 
     public boolean createPartner(Partner partner) {
-        return partnerManager.insert(getConnection(), partner);
+//        return partnerManager.insert(getConnection(), partner);
+        return partnerManager.insert(conn, partner);
     }
 
     public Collection getAllProjects() {
-        return projectManager.getAllRows(getConnection());
+//        return projectManager.getAllRows(getConnection());
+        return projectManager.getAllRows(conn);
     }
 
     public boolean editProject(Project project) {
-        return projectManager.update(getConnection(), project);
+//        return projectManager.update(getConnection(), project);
+        return projectManager.update(conn, project);
     }
 
     public Collection<Partner> getAllPartners() {
-        return partnerManager.getAllRows(getConnection());
+//        return partnerManager.getAllRows(getConnection());
+        return partnerManager.getAllRows(conn);
     }
 
     public boolean createPOE(POE poe) {
-        return poeManager.insert(getConnection(), poe);
+//        return poeManager.insert(getConnection(), poe);
+        return poeManager.insert(conn, poe);
     }
 
     public boolean createUserInfo(UserInfo ui) {
-        return userInfoManager.insert(getConnection(), ui);
+//        return userInfoManager.insert(getConnection(), ui);
+        return userInfoManager.insert(conn, ui);
     }
 
     public UserInfo getUserInfo(int userid) {
-        return userInfoManager.getRow(getConnection(), userid);
+//        return userInfoManager.getRow(getConnection(), userid);
+        return userInfoManager.getRow(conn, userid);
     }
 
     public UserAuthentication getUserAuthentication(String username) {
-        return userAthMan.getRow(getConnection(), username);
+//        return userAthMan.getRow(getConnection(), username);
+        return userAthMan.getRow(conn, username);
     }
 
     public boolean createUserAth(UserAuthentication ua) {
-        return userAthMan.insert(getConnection(), ua);
+//        return userAthMan.insert(getConnection(), ua);
+        return userAthMan.insert(conn, ua);
     }
 
 } // End of Class :: DBFacade
